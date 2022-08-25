af81a1e5e260d90cd909805424bfe6f622e71321
Imaad Zaffar <56759143+imaadzaffar@users.noreply.github.com>
2022-08-25 18:51:47 -0400

    Modified PatchDataset class to return orig, aug

--- 2-dagan/datasets/dataset.py
+++ 2-dagan/datasets/dataset.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, division
 import os
 import pandas as pd
+import numpy as np
 
 import torch
 from torch.utils.data import Dataset
@@ -10,6 +11,7 @@ class PatchDatasetFactory:
 
     def __init__(self,
         data_dir, 
+        csv_path,
         split_dir, 
         seed = 7, 
         print_info = True
@@ -26,6 +28,7 @@ class PatchDatasetFactory:
 
         #---> self
         self.data_dir = data_dir
+        self.labels = pd.read_csv(csv_path)
         self.split_dir = split_dir
         self.seed = seed
         self.print_info = print_info
@@ -43,23 +46,27 @@ class PatchDatasetFactory:
             print('Init patch dataset factory...')
             print("number of cases {}".format(0))
 
-    def return_splits(self, args, csv_path: str=None):
-        assert csv_path 
-        all_splits = pd.read_csv(csv_path)
-        train_split = self._get_split_from_df(args, all_splits=all_splits, split_key='train')
-        val_split = self._get_split_from_df(args, all_splits=all_splits, split_key='val')
-        test_split = self._get_split_from_df(args, all_splits=all_splits, split_key='test')
+    def return_splits(self, fold_id):
+        all_splits = pd.read_csv(os.path.join(self.split_dir, 'splits_{}.csv'.format(fold_id)))
+        train_split = self._get_split_from_df(all_splits=all_splits, split_key='train')
+        val_split = self._get_split_from_df(all_splits=all_splits, split_key='val')
+        test_split = self._get_split_from_df(all_splits=all_splits, split_key='test')
 
         return train_split, val_split, test_split
 
-    def _get_split_from_df(self, args, all_splits: dict={}, split_key: str='train', scaler=None):
+    def _get_split_from_df(self, all_splits: dict={}, split_key: str='train', scaler=None):
         split = all_splits[split_key]
         split = split.dropna().reset_index(drop=True)
+        split = list(split.values)
+
+        # get all the labels whose case ID is in split
+        labels = self.labels[self.labels['image_id'].isin(split)]
 
         if len(split) > 0:
             #---> create patch dataset
             split_dataset = PatchDataset(
                 data_dir=self.data_dir,
+                labels=labels,
                 num_classes=self.num_classes,
             )
         else:
@@ -90,9 +97,14 @@ class PatchDataset(Dataset):
         Load a pair of patch embeddings. 
         """
         path = os.path.join(self.data_dir, 'pt_files', '{}.pt'.format(slide_id.rstrip('.svs')))
-        images = torch.load(path)
-        original = images[0]
-        augmentation = images[1]  # @TODO: randomly fetch an augmentation. 
+        patch_embs = torch.load(path)
+
+        original = patch_embs[:, 0, :]  # get the original patch embedding
+
+        patch_indices = np.arange(patch_embs.shape[0])
+        aug_indices = np.random.randint(low=6, high=10, size=patch_embs.shape[0])  # get random mixed augmentation for each patch
+        augmentation = patch_embs[patch_indices, aug_indices, :]
+
         return original, augmentation 
     
     def __len__(self):
