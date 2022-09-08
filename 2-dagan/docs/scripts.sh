e1f7c600894ee43fd90f8158749029380f3c6211
Guillaume Jaume <guillaume.jaume@epfl.ch>
2022-09-08 16:47:42 -0400

    Updated script

--- 2-dagan/docs/scripts.sh
+++ 2-dagan/docs/scripts.sh
@@ -56,3 +56,18 @@ CUDA_DEVICES=1 python main.py \
     --model_type=transformer \
     --n_heads=2 \
     --emb_dim=2
+
+
+CUDA_DEVICES=0 python main.py \
+    --data_root_dir /media/disk2/proj_embedding_aug/extracted_mag40x_patch256_fp/resnet50_trunc_pt_patch_features/ \
+    --csv_fpath datasets_csv/labels.csv \
+    --split_dir splits/sicapv2/ \
+    --results_dir sicapv2/ \
+    --max_epochs=200 \
+    --lr=0.001 \
+    --batch_size=64 \
+    --drop_out=0.2 \
+    --early_stopping \
+    --model_type=transformer \
+    --n_heads=4 \
+    --emb_dim=64
